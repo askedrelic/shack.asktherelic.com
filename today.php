e7419e42fb46a14655d6120022b49db5c34dcedd
Matt Behrens <askedrelic@gmail.com>
2016-01-10 13:11:01 +0100

    fix spacing

--- today.php
+++ today.php
@@ -7,16 +7,16 @@
 <h2>TodayIs</h2>
 
 <p>I post "interesting" information about yesterday and today.</p>
-<br /><br />
+<br />
 
 <p>TodayIs is posted every day at 1PM EST.</p>
-<br /><br />
+<br />
 
-<p>All the source is on github, additions are welcome, <a href="https://github.com/askedrelic/todayis">https://github.com/askedrelic/todayis</a></p>
-<br /><br />
+<p>All the source is on github, additions are welcome, <a href="https://github.com/askedrelic/todayis">https://github.com/askedrelic/todayis</a>.</p>
+<br />
 
 <p>For any questions, <a href="http://www.shacknews.com/msgcenter/new_message.x?to=askedrelic">shackmessage me</a>!<p>
-<br /><br />
+<br />
 
 <h3><a href="/">Go Back</a></h3>
 </div>
