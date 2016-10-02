0b5eae9ae7134f36b387a2db0981c4d9a9681695
Matt Behrens <askedrelic@gmail.com>
2016-10-02 23:09:12 +0200

    use https approved urls

--- global.php
+++ global.php
@@ -6,7 +6,7 @@ $html = '<html>
 <title>AskTheRelic.com | '.$title.'</title>
 <style type="text/css">@import "/index.css";</style>
 <style type="text/css">@import "/redmond.css";</style>
-<script src="http://www.google.com/jsapi"></script>
+<script src="//www.google.com/jsapi"></script>
 <script type="text/javascript">
   google.load("jquery", "1.3.2");
   google.load("jqueryui", "1.7.1");
@@ -18,7 +18,7 @@ $html .= '</head>
 <div id="container">
 
 <div class="section" id="header">
-<h1><a href="http://www.asktherelic.com" alt="www.AskTheRelic.com">AskTheRelic.com</a></h1>
+<h1><a href="https://www.asktherelic.com" alt="www.AskTheRelic.com">AskTheRelic.com</a></h1>
 </div>';
 return $html;
 }
@@ -26,7 +26,7 @@ return $html;
 function getFooter($google=NULL) {
 if($google) {
 $html = '</div>
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+<script src="//www.google-analytics.com/urchin.js" type="text/javascript">
 </script>
 <script type="text/javascript">
 _uacct = "UA-1004202-1";
